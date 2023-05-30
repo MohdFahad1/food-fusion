18c8554193c5645a42d4ddbec5fee4cec5ead260
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-30 19:21:51 +0530

    Footer completed

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import logo from "../../Assets/logo.png";
+import "./Footer.css";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <p className="footer-para">
+        We're a tream of Professional cooks who are excited about their food,
+        amazing skills and expertise in cooking.
+      </p>
+      <div className='logo'>
+      <div className='image-logo'>
+        <img src={logo} alt="logo" />
+        <h1>Taste</h1>
+      </div>
+      <p>Restaurant & BBQ</p>
+      </div>
+    </footer>
+  );
 }
-
-export default Footer
