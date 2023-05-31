39951c563345b5a9ec56502a9e532adb6b9a9022
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-31 12:57:19 +0530

    api added

---
+++ src/pages/Menu/data.js
@@ -0,0 +1,141 @@
+import img1 from '../../Assets/bigBurger.png';
+import img2 from '../../Assets/bowl1.png';
+import img3 from '../../Assets/bowl2.png';
+import img4 from '../../Assets/bowl3.png';
+import img5 from '../../Assets/bowl4.png';
+import img6 from '../../Assets/fruits.png';
+import img7 from '../../Assets/granola cereal bowl.png';
+import img8 from '../../Assets/kisspng-aa-na-tigela-smoothie-aa-palm-bowl-apple-j-goji-berry-5b306ffe7baf07.9992682215299010545066.png';
+import img9 from '../../Assets/kisspng-breakfast-cereal-pizza-muesli-meze-vegetarian-cuis-granola-5ac48d852aeb69.1891892315228307251758.png';
+import img10 from '../../Assets/kisspng-cuisine-of-hawaii-l-l-hawaiian-barbecue-shrimp-gam-frozen-chicken-5b1e62f3a9e035.2766282415287180676958.png';
+import img11 from '../../Assets/kisspng-fried-chicken-lemon-chicken-chicken-meat-fine-dining-chicken-meat-5a940a1b75aa22.821205431519651355482.png';
+import img12 from '../../Assets/kisspng-greek-salad-fruit-salad-spinach-salad-muesli-vegetable-salad-5a96f429ce9af3.7299622115198423458463.png';
+import img13 from '../../Assets/kisspng-hamburger-street-food-seafood-fast-food-delicious-food-5a75083cceaf41.2676743415176192608466.png';
+import img14 from '../../Assets/kisspng-israeli-salad-european-cuisine.png';
+import img15 from '../../Assets/kisspng-italian-cuisine-pizza-pasta-food-plate-healthy-food-5abb864701e2b7.3892346415222390470077.png';
+import img16 from '../../Assets/pizza.png';
+import img17 from '../../Assets/kisspng-aa-na-tigela-smoothie-breakfast-juice-health-s-acai-bowl-5b11d2604db908.6695630315278946243184.png'
+
+
+
+export const menuItemsData = [
+    {
+      id: 1,
+      name: 'Mediterranean  Salad',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img1,
+    },
+    {
+      id: 2,
+      name: 'Summer Asian Slaw',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img2,
+    },
+    {
+      id: 3,
+      name: 'Burger',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img3,
+    },
+    {
+      id: 4,
+      name: 'white sauce pasta',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img4,
+    },
+    {
+      id: 5,
+      name: 'butterfly pasta',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img5,
+    },
+    {
+      id: 6,
+      name: 'tooty fruity bowl',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img6,
+    },
+    {
+      id: 7,
+      name: 'granola cereal bowl',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img7,
+    },
+    {
+      id: 8,
+      name: 'palm bowl',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img8,
+    },
+    {
+      id: 9,
+      name: 'tigela smoothie',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img9,
+    },
+    {
+      id: 10,
+      name: 'breakfast cereal',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img10,
+    },
+    {
+      id: 11,
+      name: 'cuisine of hawaii',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img11,
+    },
+    {
+      id: 12,
+      name: 'fried chicken',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img12,
+    },
+    {
+      id: 13,
+      name: 'greek salad',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img13,
+    },
+    {
+      id: 14,
+      name: 'hamburger',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img14,
+    },
+    {
+      id: 15,
+      name: 'israeli salad',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img15,
+    },
+    {
+      id: 16,
+      name: 'Pizza Pasta',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img16,
+    },
+    {
+      id: 17,
+      name: 'Pizza',
+      info: 'spicey with garlic and then deep fried to crispy perfection',
+      price: 24,
+      img: img17,
+    },
+  ];
